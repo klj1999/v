35fb11fb665e763a944aaa9b9bec38064fe1e903
klj1999 <31208398+klj1999@users.noreply.github.com>
2022-05-20 08:12:21 +0800

    Update freev2.py

--- utils/freev2.py
+++ utils/freev2.py
@@ -19,6 +19,12 @@ sites=[
         "reg_url":"https://www.ckcloud.xyz/api/v1/passport/auth/register",
         "sub":"https://www.ckcloud.xyz/api/v1/client/subscribe?token={token}"
     }
+    {
+        "name":"rocket",
+        "url":"https://rocketcloud.xyz/",
+        "reg_url":"https://rocketcloud.xyz/api/v1/passport/auth/register",
+        "sub":"https://rocket-sub.ddnsnode.com/api/v1/client/subscribe?token={token}"
+    }
 ]
 
 class tempsite():
